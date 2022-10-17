cf507ad369a79a831149abde5073ee1686fc216c
ashwin-raj03 <101547786+ashwin-raj03@users.noreply.github.com>
2022-10-18 00:11:43 +0530

    Delete Assignment/Team Member 2/Assignment2 directory

--- Assignment/Team Member 2/Assignment2/ASSINGMENT 2.py
+++
@@ -1,9 +0,0 @@
-import random
-def detect(temp,humi):
-	    if(humi<30):
-	        if(temp>30):
-	            print("Humidity : ",humi,"Temperature :",temp,"Alarm Turned On")
-for i in range(0,15):
-	    temp=random.randint(14,50)
-	    humi=random.randint(15,50)
-	    detect(temp,humi)
